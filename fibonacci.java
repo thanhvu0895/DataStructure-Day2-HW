e8a035947311e8b66665ca9a63319b5b1fe037de
Thanh Vu <75138396+thanhvu0895@users.noreply.github.com>
2022-10-31 11:47:01 -0400

    Update fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -4,7 +4,8 @@ public class fibonacci {
 	public static void main(String[] args) {
 		int input = 24;
 		
-		System.out.println(fib(input));
+		System.out.println("fib(input):  " + fib(input));
+		System.out.println("fibRecursive(input, new int[input + 1]): " + fibRecursive(input, new int[input + 1]));
 	}
 	
 	public static int fib (int n) {
@@ -21,6 +22,18 @@ public class fibonacci {
 		 
 		//Once we reached the last index, return the fibonnaci number in that index
 		return memory[n];
+	}
+	
+	public static int fibRecursive(int n, int[] memory) {
+		
+		if (n == 0 || n == 1) {
+			return n;
+		} else if (memory[n] != 0) {
+			return memory[n];
+		} else {
+			memory[n] = fibRecursive(n - 1, memory) + fibRecursive(n - 2, memory);
+			return memory[n];
+		}
 	}
 }
 
